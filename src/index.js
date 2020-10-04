b0c04a8000c51f1b0c7cb83ede52b3e030946292
kaga <kaga.fromearth@gmail.com>
2020-10-05 01:41:12 +0800

    chore: more debug info

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ const express = require('express')
 const http = require('http')
 const os = require('os')
 const app = express()
-const port = process.env.PORT
-const server = http.createServer(app);
+const {
+  PORT,
+  LOG_RESPONSE_BODY = true,
+  LOG_CONNECTION_INTERVAL = 0,
+  RESONSE_LATENCY = 0,
+  CONNECTION_CLOSE = false,
+  SERVER_KEEPALIVE_TIMEOUT = 5 * 1000
+} = process.env
+
+const server = http.createServer(app)
+server.keepAliveTimeout = SERVER_KEEPALIVE_TIMEOUT
 
 const getConnections = async () => {
   return (new Promise( (resolve, reject) => {
@@ -17,7 +26,8 @@ const getConnections = async () => {
   }))
 }
 
-app.get('/', async (req, res) => {
+let handledRequest = 0
+app.all('/', async (req, res) => {
   const {path, headers, method, body, cookies, fresh, hostname,
     ip, ips, protocol, query, subdomains, xhr} = req
     const echo = {path, headers, method, body, cookies, fresh, hostname,
@@ -30,14 +40,25 @@ app.get('/', async (req, res) => {
         }
     }
     const responseBody = {...echo, ...more}
-//    console.log(responseBody)
-    res.json(responseBody)
+    if (LOG_RESPONSE_BODY) {
+      console.log(responseBody)
+    }
+    setTimeout(() => {
+      handledRequest++
+      if (CONNECTION_CLOSE){
+        res.set('connection', 'close')
+      }
+      res.json(responseBody)
+    }, RESONSE_LATENCY)
 })
 
-setInterval(async() => {
-  console.log(await getConnections())
-}, 500)
+if (LOG_CONNECTION_INTERVAL) {
+  setInterval(async() => {
+    const connections = await getConnections()
+    console.log({connections, handledRequest})
+  }, LOG_CONNECTION_INTERVAL)
+}
 
-server.listen(port, '0.0.0.0', () => {
-    console.log(`app listening at http://0.0.0.0:${port}`)
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`app listening at http://0.0.0.0:${PORT}`)
 })
